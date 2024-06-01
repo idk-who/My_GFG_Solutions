7312833c6de4464d8f05dca623dc85898182be8d
idk-who <idontknowwhodoi@gmail.com>
2024-06-01 22:33:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Odd Even Problem/odd-even-problem.py
@@ -0,0 +1,35 @@
+
+class Solution:
+    def oddEven(self, s : str) -> str:
+        freq = [0]*26
+        
+        for i in range(len(s)):
+            freq[ord(s[i]) - ord('a')] += 1
+                
+        nx = 0
+        ny = 0
+        
+        for i in range(26):
+            if i & 1 and freq[i] > 0 and not freq[i] & 1:
+                nx += 1
+            if not i & 1 and freq[i] & 1:
+                ny += 1
+                
+        return "EVEN" if (nx + ny) % 2 == 0 else "ODD"
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        s = (input())
+
+        obj = Solution()
+        res = obj.oddEven(s)
+
+        print(res)
+
+# } Driver Code Ends
