39630fd28b9638e3088848173e80ec0593f3a392
idk-who <idontknowwhodoi@gmail.com>
2024-06-14 15:03:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Armstrong Numbers/armstrong-numbers.py
@@ -0,0 +1,19 @@
+#User function Template for python3
+
+class Solution:
+    def armstrongNumber (self, n):
+        return "true" if sum(map(lambda x: int(x)**3, list(str(n)))) == n else "false"
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = input()
+        n = int(n)
+        ob = Solution()
+        print(ob.armstrongNumber(n))
+
+# } Driver Code Ends
