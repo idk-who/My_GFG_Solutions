46c8bd44d2ae5f7cd45e5491586724cc5d98d5f0
idk-who <idontknowwhodoi@gmail.com>
2024-07-12 20:23:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
@@ -3,7 +3,53 @@
 from collections import defaultdict
 
 class Solution:
-    def isSubsetSum (self, N, arr, s):
+    def isSubsetSum(self, N, arr, s):
+        dp = 1
+        
+        for n in arr:
+            dp |= (dp << n)
+        
+        return (dp & (1 << s)) != 0
+        
+    def spOp_isSubsetSum(self, N, arr, s):
+        dp = [False]*(s+1)
+        dp[0] = True
+        if arr[0] <= s:
+            dp[arr[0]] = True
+        
+        new_dp = [False]*(s+1)
+        new_dp[0] = True
+        
+        for i in range(1, N):
+            for t in range(1, s+1):
+                notTaken = dp[t]
+                taken = dp[t-arr[i]] if arr[i]<=t else False
+                new_dp[t] = notTaken or taken
+            dp[:] = new_dp
+        
+        return dp[-1]
+        
+    
+    def tab_isSubsetSum(self, N, arr, s):
+        dp = [[False]*(s+1) for _ in range(N)]
+        
+        dp[0][0] = True
+        if arr[0] <= s:
+            dp[0][arr[0]] = True
+            
+        for i in range(1, N):
+            dp[i][0] = True
+            for t in range(1, s+1):
+                notTaken = dp[i-1][t]
+                
+                taken = dp[i-1][t-arr[i]] if arr[i] <= t else False
+                
+                dp[i][t] = taken or notTaken
+        
+        return dp[-1][-1]
+            
+        
+    def mem_isSubsetSum(self, N, arr, s):
         dp = defaultdict(bool)
         def rec(N, arr, s, ptr):
             if s == 0:
