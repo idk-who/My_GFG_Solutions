a755df07a7db62725da748016ea35f69665de461
idk-who <idontknowwhodoi@gmail.com>
2024-09-02 22:18:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum Cost Path/minimum-cost-path.py
+++ Difficulty: Hard/Minimum Cost Path/minimum-cost-path.py
@@ -1,29 +1,29 @@
-from heapq import heappush, heappop
+from heapq import heappop, heappush
 
 class Solution:
 	def minimumCostPath(self, grid):
-		n, m = len(grid), len(grid[0])
-		
-		dist = [[float("inf")]*m for _ in range(n)]
-		dist[0][0] = grid[0][0]
-		
-		h = [(dist[0][0], 0, 0)]
-		
-		while h:
-		    d, i, j = heappop(h)
-		    
-		    for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-		        ni, nj = i+di, j+dj
-		        
-		        if 0 <= ni < n and 0 <= nj < m:
-		            if d + grid[ni][nj] < dist[ni][nj]:
-		                dist[ni][nj] = d + grid[ni][nj]
-		                heappush(h, (d + grid[ni][nj], ni, nj))
-	    
-	    return dist[-1][-1]
-		 
+	    n = len(grid)
+        
+        dist = [[float("inf")]*n for _ in range(n)]
+        
+        h = []
+        h = [(grid[0][0], 0, 0)] # d, x, y
+        dist[0][0] = grid[0][0]
+        
+        while h:
+            d, x, y = heappop(h)
+            
+            if x == n-1 and y == n-1: return d
+            
+            for dx, dy in [(-1, 0), (+1, 0), (0, -1), (0, +1)]:
+                nx, ny = x+dx, y+dy
+                if 0 <= nx < n and 0 <= ny < n:
+                    if d+grid[nx][ny] < dist[nx][ny]:
+                        dist[nx][ny] = d+grid[nx][ny]
+                        heappush(h, (dist[nx][ny], nx, ny))
 
-
+        return dist[-1][-1]
+        
 #{ 
  # Driver Code Starts
 
