2e49c0914722dc4cccb26f0b36972a4b6d720cdc
idk-who <idontknowwhodoi@gmail.com>
2024-10-09 23:05:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked List Matrix/linked-list-matrix.py
@@ -0,0 +1,85 @@
+#User function Template for python3
+
+'''
+
+class Node():
+    def __init__(self,x):
+        self.data = x
+        self.right = None
+        self.down=None
+
+'''
+
+class Solution:
+    def constructLinkedMatrix(self, mat):
+        n = len(mat)
+        dummy = Node(-1)
+        temp = dummy
+        dp = dict()
+        dp[(n, n)] = None
+        
+        def rec(i, j):
+            if i == n or j == n:
+                return None
+            if (i, j) in dp:
+                temp = dp[(i, j)]
+            else:
+                temp = Node(mat[i][j])
+            temp.down = rec(i+1, j)
+            temp.right = rec(i, j+1)
+            dp[(i, j)] = temp
+            return temp
+        
+        return rec(0, 0)
+
+
+#{ 
+ # Driver Code Starts
+class Node():
+
+    def __init__(self, x):
+        self.data = x
+        self.right = None
+        self.down = None
+
+
+def display(head):
+    Dp = head
+    while Dp:
+        Rp = Dp
+        while Rp:
+            print(Rp.data, end=" ")
+            if Rp.right:
+                print(Rp.right.data, end=" ")
+            else:
+                print("null", end=" ")
+            if Rp.down:
+                print(Rp.down.data, end=" ")
+            else:
+                print("null", end=" ")
+            Rp = Rp.right
+        Dp = Dp.down
+
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        # First row input
+        a = list(map(int, input().strip().split()))
+        n = len(a)
+
+        # Input the matrix
+        mat = [a]
+        for i in range(1, n):
+            row = list(map(int, input().strip().split()))
+            mat.append(row)
+
+        # Create a Solution object and construct the linked matrix
+        obj = Solution()
+        head = obj.constructLinkedMatrix(mat)
+        if head is None:
+            print(-1)
+            continue
+        display(head)
+        print()
+
+# } Driver Code Ends
