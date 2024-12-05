b5585373b2c5ef4352b3b6cd268f81daa28be6db
idk-who <idontknowwhodoi@gmail.com>
2024-12-05 22:19:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -1,27 +1,32 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
 class Solution:
     # Function to sort an array of 0s, 1s, and 2s
     def sort012(self, arr):
-        n = len(arr)
-        p0 = 0 
-        p1 = 0
-        p2 = n-1
-        
-        while p1 <= p2:
-            if arr[p1] == 0:
-                arr[p0], arr[p1] = arr[p1], arr[p0]
-                p0 += 1
-                p1 += 1
-            elif arr[p1] == 1:
-                p1 += 1
+        zero, one, two = 0, 0, 0
+        for i in arr:
+            if i == 0:
+                zero += 1
+            elif i == 1:
+                one += 1
             else:
-                arr[p2], arr[p1] = arr[p1], arr[p2]
-                p2 -= 1
+                two += 1
+        ptr = 0
+        for i in range(zero):
+            arr[ptr] = 0
+            ptr += 1
+        for i in range(one):
+            arr[ptr] = 1
+            ptr += 1
+        for i in range(two):
+            arr[ptr] = 2
+            ptr += 1
         
-        return arr
-
 
 #{ 
- # Driver Code Starts
+ # Driver Code Starts.
 def main():
     t = int(input().strip())  # Read the number of test cases
     ob = Solution()
@@ -34,8 +39,8 @@ def main():
         ob.sort012(arr)  # Sort the array
 
         print(' '.join(map(str, arr)))  # Print the sorted array
-
-
+        print("~")
+        
 if __name__ == "__main__":
     main()
 
