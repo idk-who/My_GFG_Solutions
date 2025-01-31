b349e3d1a1aa468b2c9b789e12c39241679d914a
idk-who <idontknowwhodoi@gmail.com>
2025-01-31 22:20:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.py
+++ Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.py
@@ -3,65 +3,73 @@
 class Solution:
     
     #Function to find a solved Sudoku. 
-    def SolveSudoku(self,grid):
-        def solve(grid):
-            for i in range(9):
-                for j in range(9):
-                    if grid[i][j] == 0:
-                        for c in range(1, 10):
-                            if is_valid(grid, i, j, c):
-                                grid[i][j] = c
-                                if solve(grid):
-                                    return True
-                                grid[i][j] = 0
-                        return False
-            
-            return True
-            
+    def solveSudoku(self, mat):
+        rows = [[False]*10 for i in range(10)]
+        cols = [[False]*10 for i in range(10)]
+        blocks = [
+            [
+                [False]*10 for i in range(3)
+            ] for j in range(3)
+        ]
         
-        def is_valid(grid, i, j, c):
-            for r in range(9):
-                if grid[r][j] == c or grid[i][r] == c: return False
-            
-            ni = i//3 * 3
-            nj = j//3 * 3
-            for ti in range(ni, ni+3):
-                for tj in range(nj, nj+3):
-                    if grid[ti][tj] == c:
-                        return False
-            return True
+        for i in range(9):
+            for j in range(9):
+                ele = mat[i][j]
+                rows[i][ele] = True
+                cols[j][ele] = True
+                blocks[i//3][j//3][ele] = True
         
-        return solve(grid)
-                        
-    
-    #Function to print grids of the Sudoku.    
-    def printGrid(self,arr):
         
-        for i in arr:
-            print(*i, end = " ")
+        def rec(mat, i, j):
+            if i == 9:
+                return True
+            if mat[i][j] == 0:
+                for ele in range(1, 10):
+                    if rows[i][ele] == False and cols[j][ele] == False and blocks[i//3][j//3][ele] == False:
+                        rows[i][ele] = True
+                        cols[j][ele] = True
+                        blocks[i//3][j//3][ele] = True
+                        mat[i][j] = ele
+                        if rec(mat, i, j):
+                            return True
+                        mat[i][j] = 0
+                        rows[i][ele] = False
+                        cols[j][ele] = False
+                        blocks[i//3][j//3][ele] = False
+            else:
+                if j == 8:
+                    return rec(mat, i+1, 0)
+                else:
+                    return rec(mat, i, j+1)
+        rec(mat, 0, 0)
+        return mat
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
-if __name__=="__main__":
-    t = int(input())
-    while(t>0):
-        grid = [[0 for i in range(9)] for j in range(9)]
-        row = [int(x) for x in input().strip().split()]
-        k = 0
-        for i in range(9):
-            for j in range(9):
-                grid[i][j] = row[k]
-                k+=1
-                
-        ob = Solution()
-            
-        if(ob.SolveSudoku(grid)==True):
-            ob.printGrid(grid)
+if __name__ == '__main__':
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+
+    t = int(data[0])
+    index = 1
+
+    for _ in range(t):
+        matrix = []
+        n = 9
+        for i in range(n):
+            row = list(map(int, data[index:index + n]))
+            matrix.append(row)
+            index += n
+        obj = Solution()
+        obj.solveSudoku(matrix)
+        for i in range(n):
+            for j in range(n):
+                print(matrix[i][j], end=" ")
             print()
-        else:
-            print("No solution exists")
-        t = t-1
+        print("~")
+
 # } Driver Code Ends
