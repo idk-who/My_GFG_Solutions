f56dd4caa640ba2620a6201dec6d9175a787ebda
idk-who <idontknowwhodoi@gmail.com>
2025-06-10 21:26:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Exactly one swap/exactly-one-swap.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countStrings(self, s): 
+        n=len(s)
+        freq=[0]*26
+        for item in s:
+            freq[ord(item)-ord("a")]+=1
+        ans=(n*(n-1)//2)
+        flag=False
+        for val in freq:
+            if val>1:
+                ans-=(val*(val-1)//2)
+                flag=True
+        if flag:
+            ans+=1
+        return ans
