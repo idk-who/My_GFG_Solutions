8c7422b4bf4acd3a0f6836570c10b11ee7ddeac6
idk-who <idontknowwhodoi@gmail.com>
2025-07-04 22:07:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays With At Most K Distinct Integers/subarrays-with-at-most-k-distinct-integers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countAtMostK(self, arr, k):
+        freq={}
+        ans=0
+        left=0
+        for right in range(len(arr)):
+            freq[arr[right]]=freq.get(arr[right],0)+1
+            while len(freq)>k:
+                freq[arr[left]]-=1
+                if freq[arr[left]]==0:
+                    del freq[arr[left]]
+                left+=1
+            ans+=(right-left+1)
+        return ans
