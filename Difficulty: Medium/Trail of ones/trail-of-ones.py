203c0f5ea615aacf0d6366c66aed56001b559b76
idk-who <idontknowwhodoi@gmail.com>
2025-07-11 23:37:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Trail of ones/trail-of-ones.py
+++ Difficulty: Medium/Trail of ones/trail-of-ones.py
@@ -1,28 +1,6 @@
-#User function Template for python3
 class Solution:
-    def numberOfConsecutiveOnes (ob,n):
-        # calc string that does not have consecutive 1's
-        
-        dp = [[0, 0] for _ in range(n+1)]
-        dp[1] = [1, 1]
-        
-        for i in range(2, n+1):
-            dp[i] = [(dp[i-1][0]+dp[i-1][1]) % (10**9+7), dp[i-1][0]]
-        
-        return ((1<<n)-(dp[n][0] + dp[n][1])) % (10**9+7)
-        
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-
-        N = int(input())
-
-        ob = Solution()
-        print(ob.numberOfConsecutiveOnes(N))
-
-# } Driver Code Ends
+    def countConsec(self, n: int) -> int:
+        one,zero=1,1
+        for i in range(2,n+1):
+            one,zero=zero,zero+one
+        return 2**n-one-zero
