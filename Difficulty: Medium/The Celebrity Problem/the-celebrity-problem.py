aa8973377a48091b1afaa4c47a45fe3a471c97e2
idk-who <idontknowwhodoi@gmail.com>
2025-08-30 23:57:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.py
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.py
@@ -1,24 +1,13 @@
 class Solution:
     def celebrity(self, mat):
-        for i in range(len(mat)):
-            if sum(mat[i]) == 0:
-                if sum([mat[j][i] for j in range(len(mat))]) == len(mat)-1:
-                    return i
-        return -1
-
-
-#{ 
- # Driver Code Starts
-# Main function to handle input and output
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        n = int(input())
-        M = []
-        for _ in range(n):
-            M.append(list(map(int, input().split())))
-
-        ob = Solution()
-        print(ob.celebrity(M))
-
-# } Driver Code Ends
+        n=len(mat)
+        a,b=0,n-1
+        while a<b:
+            if mat[a][b]:
+                a+=1
+            else:
+                b-=1
+        for i in range(n):
+            if a!=i and mat[a][i] or not mat[i][a]:
+                return -1
+        return a
