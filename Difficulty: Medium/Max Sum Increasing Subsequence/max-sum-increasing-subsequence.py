00261b816602766116c506919c3bdd73d13438c3
idk-who <idontknowwhodoi@gmail.com>
2025-11-17 23:30:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Sum Increasing Subsequence/max-sum-increasing-subsequence.py
+++ Difficulty: Medium/Max Sum Increasing Subsequence/max-sum-increasing-subsequence.py
@@ -1,26 +1,7 @@
-#User function Template for python3
+from functools import cache
 class Solution:
-	def maxSumIS(self, nums, n):
-        dp = nums[:]
-        
-        for i in range(n):
-            for j in range(i+1, n):
-                if nums[j] > nums[i]:
-                    dp[j] = max(dp[j], dp[i] + nums[j])
-        
-        return max(dp)
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		n = int(input())
-		Arr = [int(x) for x in input().split()]
-		ob = Solution()
-		ans = ob.maxSumIS(Arr,n)
-		print(ans)
-
-# } Driver Code Ends
+	def maxSumIS(self, arr):
+		@cache
+		def dfs(i,prev):
+            return 0 if i==len(arr) else max(dfs(i+1,prev),arr[i]+dfs(i+1,i) if prev==-1 or arr[i]>arr[prev] else 0)
+        return dfs(0,-1)
