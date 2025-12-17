e9ae4f6a12d22705115b715935177cb60a3119c3
idk-who <idontknowwhodoi@gmail.com>
2025-12-17 23:15:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
@@ -1,41 +1,14 @@
 class Solution:
-	def mergeOverlap(self, arr):
-		arr.sort()
-		ans = []
-		start = arr[0][0]
-		end = arr[0][1]
-		for i in range(1, len(arr)):
-		    if end >= arr[i][0]:
-                end = max(end, arr[i][1])
+    def mergeOverlap(self, arr):
+        # Code here
+        arr.sort()
+        ans = []
+        running = arr[0]
+        for i in range(1, len(arr)):
+            if running[1] < arr[i][0]:
+                ans.append(running)
+                running = arr[i]
             else:
-                ans.append([start, end])
-                start = arr[i][0]
-                end = arr[i][1]
-        ans.append([start, end])
-		return ans
-
-
-#{ 
- # Driver Code Starts
-if __name__ == '__main__':
-    T = int(input())
-    for i in range(T):
-        n = int(input())
-        # a = list(map(int, input().strip().split()))
-        arr = []
-        # j = 0
-        for i in range(n):
-            a = list(map(int, input().strip().split()))
-            x = a[0]
-            # j += 1
-            y = a[1]
-            # j += 1
-            arr.append([x, y])
-        obj = Solution()
-        ans = obj.mergeOverlap(arr)
-        for i in ans:
-            for j in i:
-                print(j, end=" ")
-        print()
-
-# } Driver Code Ends
+                running = (min(running[0], arr[i][0]), max(running[1], arr[i][1]))
+              
+        return ans + [running]
