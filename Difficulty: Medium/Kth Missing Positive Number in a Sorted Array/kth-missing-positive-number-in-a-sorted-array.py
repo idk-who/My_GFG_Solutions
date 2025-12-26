4992be745acf97d78f40fcf813afa0590da81636
idk-who <idontknowwhodoi@gmail.com>
2025-12-26 22:28:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.py
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.py
@@ -1,10 +1,6 @@
-from bisect import bisect_left as bisect
-
 class Solution:
     def kthMissing(self, arr, k):
-        if arr[0] > k: return k
-        
-        lo = 1
+        lo = 0
         hi = len(arr) - 1
         
         while lo <= hi:
@@ -17,25 +13,7 @@ class Solution:
             else:
                 hi = m - 1
         
-        return hi + k + 1
+        return lo + k
+
             
-            
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-#Main
-if __name__ == '__main__':
-    t = int(input())
-    while t:
-        t -= 1
-        A = [int(x) for x in input().strip().split()]
-        nd = [int(x) for x in input().strip().split()]
-        D = nd[0]
-        ob = Solution()
-        ans = ob.kthMissing(A, D)
-        print(ans)
-        print("~")
-
-# } Driver Code Ends
