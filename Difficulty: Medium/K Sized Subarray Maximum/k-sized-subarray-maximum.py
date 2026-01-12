11929f71b6188c9a14d119f6328577c831947397
idk-who <idontknowwhodoi@gmail.com>
2026-01-12 22:08:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.py
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.py
@@ -1,52 +1,23 @@
-from heapq import heappush, heappop
-
 class Solution:
-    def max_of_subarrays(self,k,arr):
-        h = []
-        ans = []
-        for i in range(len(arr)):
-            heappush(h, (-arr[i], i))
+    def maxOfSubarrays(self, arr, k):
+        # code here
+        st=[]
+        l=len(arr)
+        ans=[0 for _ in range(l-k+1)]
+        def pus(st,ele):
+            while st and st[-1]<ele:
+                st.pop()
+            st.append(ele)
             
-            if i+1 >= k:
-                while h[0][1] <= i-k:
-                    heappop(h)
-                ans.append(-h[0][0])
+        def po(st,ele):
+            if st and st[0]==ele:
+                st.pop(0)
+                
+        for i in range(k-1):
+            pus(st,arr[i])
             
+        for i in range(k-1,l):
+            pus(st,arr[i])
+            ans[i-k+1]=st[0]
+            po(st,arr[i-k+1])
         return ans
-        
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-import atexit
-import io
-import sys
-from collections import deque
-
-#Contributed by : Nagendra Jha
-
-_INPUT_LINES = sys.stdin.read().splitlines()
-input = iter(_INPUT_LINES).__next__
-_OUTPUT_BUFFER = io.StringIO()
-sys.stdout = _OUTPUT_BUFFER
-
-
-@atexit.register
-def write():
-    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
-
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        k = int(input())
-        arr = list(map(int, input().strip().split()))
-        ob = Solution()
-        res = ob.max_of_subarrays(k, arr)
-        for i in range(len(res)):
-            print(res[i], end=" ")
-        print()
-
-# } Driver Code Ends
