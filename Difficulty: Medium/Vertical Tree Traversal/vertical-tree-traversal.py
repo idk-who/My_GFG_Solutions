a7d7815738730e86c0a4f2e5b55bce5a30cb86ca
idk-who <idontknowwhodoi@gmail.com>
2026-03-15 22:10:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.py
+++ Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.py
@@ -1,118 +1,21 @@
-#User function Template for python3
-
-
-from collections import defaultdict
-
 class Solution:
-    
-    #Function to find the vertical order traversal of Binary Tree.
-    def verticalOrder(self, root): 
-        d = defaultdict(list)
+    def verticalOrder(self, root):
+        if not root:
+            return []
         
-        def rec(root, d, i, j):
-            if root is None:
-                return None
+        hd_map = defaultdict(list)
+        queue = deque([(root, 0)])
+        min_hd = max_hd = 0
+        
+        while queue:
+            node, hd = queue.popleft()
+            hd_map[hd].append(node.data)
+            min_hd = min(min_hd, hd)
+            max_hd = max(max_hd, hd)
             
-            rec(root.left, d, i+1, j-1)
-            d[(j, i)].append(root.data)
-            rec(root.right, d, i+1, j+1)
-            
-        rec(root, d, 0, 0)
+            if node.left:
+                queue.append((node.left, hd - 1))
+            if node.right:
+                queue.append((node.right, hd + 1))
         
-        ans = []
-        
-        for (j, i), l in sorted(d.items()):
-            ans.extend(l)
-        
-        return ans
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-#Contributed by Sudarshan Sharma
-from collections import deque
-# Tree Node
-class Node:
-    def __init__(self, val):
-        self.right = None
-        self.data = val
-        self.left = None
-
-
-
-# Function to Build Tree   
-def buildTree(s):
-    #Corner Case
-    if(len(s)==0 or s[0]=="N"):           
-        return None
-        
-    # Creating list of strings from input 
-    # string after spliting by space
-    ip=list(map(str,s.split()))
-    
-    # Create the root of the tree
-    root=Node(int(ip[0]))                     
-    size=0
-    q=deque()
-    
-    # Push the root to the queue
-    q.append(root)                            
-    size=size+1 
-    
-    # Starting from the second element
-    i=1                                       
-    while(size>0 and i<len(ip)):
-        # Get and remove the front of the queue
-        currNode=q[0]
-        q.popleft()
-        size=size-1
-        
-        # Get the current node's value from the string
-        currVal=ip[i]
-        
-        # If the left child is not null
-        if(currVal!="N"):
-            
-            # Create the left child for the current node
-            currNode.left=Node(int(currVal))
-            
-            # Push it to the queue
-            q.append(currNode.left)
-            size=size+1
-        # For the right child
-        i=i+1
-        if(i>=len(ip)):
-            break
-        currVal=ip[i]
-        
-        # If the right child is not null
-        if(currVal!="N"):
-            
-            # Create the right child for the current node
-            currNode.right=Node(int(currVal))
-            
-            # Push it to the queue
-            q.append(currNode.right)
-            size=size+1
-        i=i+1
-    return root
-    
-    
-if __name__=="__main__":
-    t=int(input()) 
-    import sys
-    sys.setrecursionlimit(10000)
-    for _ in range(0,t):
-        s=input()
-        root=buildTree(s)
-        res = Solution().verticalOrder(root)
-        for i in res:
-            print (i, end=" ")
-        print()
-
-
-
-# } Driver Code Ends
+        return [hd_map[hd] for hd in range(min_hd, max_hd + 1)]
